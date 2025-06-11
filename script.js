8fb9a389dee3db1da0937d60916ddf07bedabdba
pushpak <pushpak.bhute12@gmail.com>
2025-06-11 21:44:19 +0530

    capcha chnges done

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-function beforesubmit(){
+let captchacheked = false;
+
+
+function beforesubmit(event){
+
+    if(captchacheked){
     let outputdate = document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate");
     console.log("input value", inputdate.value);  // string --> date (en_IN)
 
     let formatedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
     outputdate.value = formatedDate;
-
+    }else{
+        
+        alert("please check the reCAPTCHA box to SUBMIT THE LEAD");
+        event.preventDefault();
+    }
 
 }
 
@@ -20,3 +29,7 @@ function timestamp() {
              JSON.stringify(elems);
          } } 
      setInterval(timestamp, 500); 
+
+     function captchasuccess(){
+       captchacheked =true;
+     }
