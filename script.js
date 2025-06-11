f4f1368279766bdaca77f795d7381862b149248b
pushpak <pushpak.bhute12@gmail.com>
2025-06-11 20:37:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function beforesubmit(){
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log("input value", inputdate.value);  // string --> date (en_IN)
+
+    let formatedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+    outputdate.value = formatedDate;
+}
